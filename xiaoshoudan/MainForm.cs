e2827782e2aef350c693dc6ceb90134a06d81f43
1 <1@1.com>
2023-02-23 12:06:47 +0800

    first

---
+++ xiaoshoudan/MainForm.cs
@@ -0,0 +1,260 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： e
+ * 日期: 2023/2/22
+ * 时间: 18:16
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System .Data .OleDb ;
+using System .IO ;
+using NPOI.HSSF.UserModel;
+using NPOI.HPSF;
+using NPOI.POIFS.FileSystem;
+using NPOI.SS.Util;
+using NPOI.SS.UserModel;
+
+namespace xiaoshoudan
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			     
+	            
+		         
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	             OpenFileDialog fileDialog = new OpenFileDialog();
+	             fileDialog.Multiselect = false; 
+	             fileDialog.Title = "请选择文件"; 
+	             fileDialog.Filter="Excel工作簿(97-2003)|*.xls|Excel工作簿(2007以上）|*.xlsx";
+	             fileDialog.InitialDirectory = @"C:\";
+	              
+	             if (fileDialog .ShowDialog () == DialogResult.OK )
+	                 {
+	             	
+	             	label1 .Text =fileDialog .FileName ;
+	             		}
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			string h = DateTime .Now .Hour .ToString ();
+			string m =DateTime .Now .Minute .ToString ();
+			string s = DateTime .Now .Second .ToString ();
+			string y = DateTime .Now .Year.ToString ();
+			string M = DateTime .Now .Month .ToString ();
+			string d = DateTime .Now .Day .ToString ();
+			string str = string.Format ("销售单_{0}年{1}月{2}日,{3}时{4}分{5}秒",y,M,d,h,m,s);
+			string str1 = @"c:\"+str;
+			Directory .CreateDirectory (str1);
+			DataHelper helper =new DataHelper (label1 .Text.Trim () );
+	       helper.open ();
+	       string str2 = "select distinct name from [sheet1$]";
+	       OleDbDataReader reader = helper .dataread (str2);
+	       while(reader .Read ())
+	       {      	
+	       	DataHelper helper1 = new DataHelper(label1 .Text .Trim());
+	       	helper1.open ();
+	       	string str3 ="select * from [sheet1$] where name = '"+ reader[0].ToString()+"' order by date ";
+	       
+	       	OleDbDataReader reader1= helper1.dataread (str3);
+	        	
+	       	HSSFWorkbook workbook =new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet("sheet1");
+	       
+	       	
+	       sheet.SetColumnWidth(0,  (int)((17.25+0.68) *256));
+           sheet.SetColumnWidth(1,  (int)((55.75+0.78) *256));
+           sheet.SetColumnWidth(2,  (int)((6.25+0.68) *256));
+           sheet.SetColumnWidth(3,  (int)((6.83+0.78) *256));
+           sheet.SetColumnWidth(4,  (int)((13.63+0.78) *256));
+           sheet.SetColumnWidth(5,  (int)((13.88+0.78) *256));
+           sheet.SetColumnWidth(6,  (int)((16.13+0.78) *256));
+           sheet.SetColumnWidth(7,  (int)((10.38+0.78) *256));   
+	       	
+	       IRow row = sheet.CreateRow(0);
+           ICell cell=row.CreateCell(0);	
+           
+            row.Height =51*20;
+            cell.SetCellValue("销售单");
+           sheet.AddMergedRegion(new CellRangeAddress(0, 0, 0, 7));
+           ICellStyle style=workbook .CreateCellStyle ();
+           style.VerticalAlignment =VerticalAlignment .CENTER ;
+           style.Alignment = NPOI.SS.UserModel.HorizontalAlignment .CENTER ;
+           IFont font= workbook .CreateFont();
+           font.FontName ="宋体";
+           font.Boldweight =short.MaxValue ;
+           font.FontHeightInPoints =16;
+           style.SetFont (font);
+           row.GetCell(0).CellStyle =style ;
+           
+           
+           
+           
+           ICellStyle style1=workbook.CreateCellStyle();
+           style1.VerticalAlignment =VerticalAlignment .CENTER ;
+           style1.Alignment = NPOI.SS.UserModel.HorizontalAlignment .CENTER ;
+           IFont font1=workbook .CreateFont ();
+           font1.Boldweight =short .MaxValue ;
+           font1.FontHeightInPoints =12;
+           font1.FontName ="宋体";
+           style1.BorderBottom = NPOI.SS.UserModel.BorderStyle.THIN;
+           style1.BorderLeft =NPOI.SS.UserModel.BorderStyle.THIN;
+           style1.BorderRight =NPOI.SS.UserModel.BorderStyle.THIN;
+           style1.BorderTop =NPOI.SS.UserModel.BorderStyle.THIN;
+           style1 .SetFont (font1);
+            
+             IRow row1 = sheet.CreateRow(1);
+           row1.Height =23*20;
+           string[] strarr={"姓名","内容","数量","单价","金额","是否付款","日期","备注"};
+           for(int i=0;i<=7;i++){
+           ICell cell1=row1.CreateCell(i);
+           cell1.SetCellValue(strarr[i]);
+           cell1.CellStyle =style1 ;      
+                                 }
+           
+           ICellStyle style2=workbook.CreateCellStyle();
+           style2.VerticalAlignment =VerticalAlignment .CENTER ;
+           style2.Alignment = NPOI.SS.UserModel.HorizontalAlignment .CENTER ;
+           IFont font2=workbook .CreateFont ();
+           font2.FontHeightInPoints =12;
+           
+           font2.FontName ="宋体";
+           style2.BorderBottom = NPOI.SS.UserModel.BorderStyle.THIN;
+           style2.BorderLeft =NPOI.SS.UserModel.BorderStyle.THIN;
+           style2.BorderRight =NPOI.SS.UserModel.BorderStyle.THIN;
+           style2.BorderTop =NPOI.SS.UserModel.BorderStyle.THIN;
+           style2 .SetFont (font2);
+           
+           int n=0; int sl = 0; decimal hj=0; decimal jizhang=0; decimal yifukuan= 0;
+             while(reader1.Read ()){
+           	IRow row2=sheet.CreateRow(n +2);
+           	row2.Height =16*20 ;
+           	int a1 = 0; decimal  a2 = 0; decimal  a3 = 0;
+           	
+           	if (reader1[2].ToString ().Trim ()=="")
+           	  {a1=0;}
+           	else
+           	   {a1=Convert.ToInt32(reader1[2].ToString ().Trim ());}
+           	
+           	if (reader1[4].ToString ().Trim ()=="")
+           	    {a2=0;a3=0;}
+           	else 
+           	{
+           	  if(reader1 [5].ToString ().Trim ()=="记账")
+           	     { a2=Convert.ToDecimal(reader1[4].ToString ().Trim ());}
+           	  else
+                {a3=Convert.ToDecimal (reader1[4].ToString ().Trim ());}
+           	}
+           		
+           	
+           	sl =sl + a1;
+           	jizhang  = jizhang  + a2;
+           	yifukuan =yifukuan + a3;
+           	
+           	//将一条记录内容放入数组
+                       string[] strarray=new string[8];
+                       strarray[0]=reader1[0].ToString ().Trim ();
+                       strarray [1]=reader1[1].ToString ().Trim ();
+                       strarray [2]=reader1[2].ToString ().Trim ();
+                       strarray [3]=reader1[3].ToString ().Trim ();
+                       strarray [4]=reader1[4].ToString().Trim ();
+                       strarray [5]=reader1[5].ToString ().Trim ();
+                       //string[] c=reader1[6].ToString ().Split (' ');
+                       strarray [6]=String.Format("{0:yyyy-MM-dd}",reader1[6]);;
+                        strarray [7]=reader1[7].ToString ().Trim ();
+                  //在每一行建立7个单元格，赋值，并指定样式
+                  for (int a=0;a<=7;a++){
+                     	ICell cell5=row2.CreateCell(a);
+                     	cell5.SetCellValue(strarray [a]);
+                     	cell5.CellStyle =style2;
+                      	
+                  }
+                  
+                  n++;
+                 
+                  }
+                hj= jizhang + yifukuan ;
+                IRow row7=sheet.CreateRow(n +2);
+           	    row7.Height =16*20 ;
+           	    ICell cell10 = row7.CreateCell (0); cell10.CellStyle =style2;
+           	    ICell cell11 = row7.CreateCell (1);cell11.SetCellValue("合计");cell11.CellStyle =style2;
+           	    ICell cell12 = row7.CreateCell (2);cell12.SetCellValue(sl.ToString ()); cell12.CellStyle =style2;
+           	    ICell cell13 = row7.CreateCell (3);cell13.CellStyle =style2;
+           	    ICell cell14 = row7.CreateCell (4);cell14.SetCellValue(hj.ToString ());cell14.CellStyle =style2;
+           	    ICell cell15 = row7.CreateCell (5); cell15.CellStyle =style2;
+           	    ICell cell16 = row7.CreateCell (6); cell16.CellStyle =style2;
+           	    ICell cell17 = row7.CreateCell (7); cell17.CellStyle =style2;
+           	    
+           	    n++;
+           	    IRow row8=sheet.CreateRow(n +2);
+           	    row7.Height =16*20 ;
+           	    ICell cell20 = row8.CreateCell (0); cell20.CellStyle =style2;
+           	    ICell cell21 = row8.CreateCell (1); cell21.CellStyle =style2;
+           	    ICell cell22 = row8.CreateCell (2); cell22.CellStyle =style2;
+           	    ICell cell23 = row8.CreateCell (3); cell23.SetCellValue("记账");cell23.CellStyle =style2;
+           	    ICell cell24 = row8.CreateCell (4); cell24.SetCellValue(jizhang.ToString ());cell24.CellStyle =style2;
+           	    ICell cell25 = row8.CreateCell (5); cell25.CellStyle =style2;
+           	    ICell cell26 = row8.CreateCell (6); cell26.CellStyle =style2;
+           	    ICell cell27 = row8.CreateCell (7); cell27.CellStyle =style2;
+           	    
+           	    
+                n++;
+           	    IRow row9=sheet.CreateRow(n +2);
+           	    row7.Height =16*20 ;
+           	    ICell cell30 = row9.CreateCell (0); cell30.CellStyle =style2;
+           	    ICell cell31 = row9.CreateCell (1); cell31.CellStyle =style2;
+           	    ICell cell32 = row9.CreateCell (2); cell32.CellStyle =style2;
+           	    ICell cell33 = row9.CreateCell (3); cell33.SetCellValue("已付款");cell33.CellStyle =style2;
+           	    ICell cell34 = row9.CreateCell (4); cell34.SetCellValue(yifukuan .ToString ());cell34.CellStyle =style2;
+           	    ICell cell35 = row9.CreateCell (5); cell35.CellStyle =style2;
+           	    ICell cell36 = row9.CreateCell (6); cell36.CellStyle =style2;
+           	    ICell cell37 = row9.CreateCell (7); cell37.CellStyle =style2;
+           	    
+           	    FileStream fs = File.OpenWrite(str1+ "\\" +reader[0].ToString () + ".xls");
+		             workbook .Write(fs);
+		                fs.Close ();
+           
+		                helper1.close ();
+           
+                           	
+	       }
+	       
+	       helper .close ();
+	       	
+	       	MessageBox .Show("已成功生成完毕,文件位于文件夹\n" + str + "\\" );
+	    }
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ xiaoshoudan/classes.cs
@@ -0,0 +1,404 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： e
+ * 日期: 2023/2/16
+ * 时间: 18:59
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+
+using System;
+using System .Text ;
+using System .Data.OleDb ;
+using System .Data;
+using System .IO ;
+using System.Security.Cryptography;  
+using System .Collections; 
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace xiaoshoudan
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+
+	
+//对填写内容进行校验类
+public static class myvalidate{
+	
+		//1.判断是否为空
+		 public static bool ifblank(string str){
+			if (str.Trim() == null || str.Trim().Length == 0)    //验证这个参数是否为空
+			{return true;}                           //是，就返回False
+             else
+             {return false ;}
+		}
+		
+		//2.判断是否都是数字
+		public static bool myvalidateNumric(string str){
+     		 ASCIIEncoding ascii = new ASCIIEncoding();//new ASCIIEncoding 的实例
+             byte[] bytestr = ascii.GetBytes(str);         //把string类型的参数保存到数组里
+             foreach (byte c in bytestr)                   //遍历这个数组里的内容
+            {
+                if (c < 48 || c > 57)                          //判断是否为数字
+                {
+                    return false;                              //不是，就返回False
+                                    }
+            }
+            return true;                                        //是，就返回True
+        }
+		
+		
+		//3.判断是否超过某个 位数
+		 public static bool ifchaoguoweishu(string str , int n){
+			if (str.Trim().Length > n)    //验证这个参数是否大于某个位数
+                return false;                           //是，就返回False
+            else
+             	return true;    
+		}
+		
+		//4.判断是否低于某个位数
+		 public static bool ifdiyuweishu(string str , int n){
+			if (str.Trim().Length < n)    //验证这个参数是否大于某个位数
+                return false;                           //是，就返回False
+			else
+             	return true;
+            }
+				
+		//判断是否是两位小数或整数
+		public static bool ifshuozi(string str)
+		{Regex reg =new Regex("^[0-9]*(\\.[0-9]{1,2})?$");
+		
+		if (reg.IsMatch (str))
+		{
+			return true ;
+		}
+		else 
+		{
+			return false;
+		}
+		
+		
+		}
+		
+		}
+
+//读文件类
+public static class readtxt{
+
+	public static string IP="";
+		
+	public static string getIP(){
+		foreach (string line in File.ReadLines(@"IPAddress.txt"))
+                    {   
+		      //if ((line.ToString().Trim ().Substring(0,2))=="IP")
+		        //    {         
+		      	IP =(line .ToString ().Trim ());
+		    	//  }
+		   
+		}
+	   return IP;
+	
+	}
+}
+
+//数据库类
+public  class DataHelper{
+	//"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dataAddress;    
+	// @"data source="+chuandi .IPa +";initial catalog=zhanghu;user id=sa;pwd=111111";
+	
+	//创建数据库连接
+     
+     public OleDbConnection  link { get; set; }
+     public DataHelper ()
+     {
+     }
+     public DataHelper (string str)
+     {
+     link = new OleDbConnection( "provider= Microsoft.Jet.OLEDB.4.0;extended properties=Excel 8.0;data source="+ str);
+     
+     }
+	 
+		   
+     //1.打开数据库
+	   public void open(){
+            if (link.State==0)
+    	    link.Open();
+    		}
+	  
+        //2.读取数据库
+       public OleDbDataReader dataread(string selectstring ) {
+    	OleDbCommand  mycommand=link .CreateCommand ();
+        mycommand.CommandText =selectstring ;
+        OleDbDataReader myReader=mycommand.ExecuteReader ();
+        return myReader;
+      }
+    
+        //3.执行数据库命令
+        public void dataexecute(string exestr){
+          OleDbCommand  mycommand=link.CreateCommand();
+          mycommand.CommandText =exestr;
+          mycommand.ExecuteNonQuery ();
+       }
+    
+       //4.关闭数据库
+        public void close(){
+    	   if (link.State!=0)
+    	   link.Close ();
+           link =null ;
+        }
+
+       //5.填充DATAset
+        public  void FullDataSet(string sqlstr,DataSet mydataset,string TableName){
+    	OleDbDataAdapter  myDataAdapter=new OleDbDataAdapter  (sqlstr ,link);
+    	myDataAdapter.Fill (mydataset ,TableName );
+        }
+  
+}
+
+
+//传递值类
+public class chuandi{
+public static int zhi1;//级别
+public static object zhi2;//登陆窗体
+public static string zhi3;  //用户名
+public static string IPa;  
+public static bool issev; //是否是服务器
+}
+
+
+//询问类
+
+static class Ask
+{
+	static public bool YesOrNot(string str)
+	{
+		DialogResult a = MessageBox .Show(str,"注意",MessageBoxButtons .YesNo ,MessageBoxIcon .Question ,MessageBoxDefaultButton .Button1 );
+	if (a == DialogResult.No )
+	{return false;}
+	else
+	{return true;}
+	}
+}
+
+
+//添加数据库类
+
+static class  DataExec
+{
+	static public int DE(string str){
+		DataHelper helper1=new DataHelper ();
+	   try {
+			helper1 .open();
+			helper1 .dataexecute(str);
+			return 1 ;
+			}
+			catch{
+			MessageBox .Show("错误");
+			return 2;
+		      }
+			finally {
+			helper1.close ();
+			}
+	}
+}
+
+}
+
+
+
+//md5加密类
+public class md5c{
+
+	public  static string MD5Create(string STR) //STR为待加密的string
+             {
+            string pwd = "";
+            //pwd为加密结果
+            MD5 md5 = MD5.Create();
+            byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(STR));
+            //这里的UTF8是编码方式，你可以采用你喜欢的方式进行，比如UNcode等等
+            for (int i = 0; i < s.Length; i++)
+            {
+                pwd = pwd + s[i].ToString();
+            }
+            return pwd;
+        }
+
+}
+
+
+
+
+///取汉字首字母类
+ static class GetFirstLetter
+ {
+/// <summary>
+ 
+/// 在指定的字符串列表CnStr中检索符合拼音索引字符串
+ 
+///</summary>
+ 
+/// <param name="CnStr">汉字字符串</param>
+ 
+/// <returns>相对应的汉语拼音首字母串</returns>
+ 
+public static string GetSpellCode(string CnStr) {
+ 
+　  string strTemp="";
+ 
+　　int iLen=CnStr.Length;
+ 
+　　int i=0;
+ 
+　　for (i=0;i<=iLen-1;i++) {
+ 
+　　　　 strTemp+=GetCharSpellCode(CnStr.Substring(i,1));
+ 
+　　　　 
+　　}
+ 
+　　return strTemp;
+ 
+}
+ 
+/// <summary>
+ 
+/// 得到一个汉字的拼音第一个字母，如果是一个英文字母则直接返回大写字母
+ 
+/// </summary>
+ 
+/// <param name="CnChar">单个汉字</param>
+ 
+/// <returns>单个大写字母</returns>
+ 
+private static string GetCharSpellCode(string CnChar) {
+ 
+　　long iCnChar;
+ 
+　　byte[] ZW = System.Text.Encoding.Default.GetBytes(CnChar);
+ 
+　　//如果是字母，则直接返回
+ 
+　　if (ZW.Length==1) {
+ 
+　　　　 return CnChar.ToUpper();
+ 
+　　}
+ 
+　　else {
+ 
+　　　　 // get the array of byte from the single char
+ 
+　　　　int i1 = (short)(ZW[0]);
+ 
+　　　　int i2 = (short)(ZW[1]);
+ 
+　　　　iCnChar = i1*256+i2;
+ 
+　　　　}　　
+ 
+// iCnChar match the constant
+ 
+　　if ((iCnChar>=45217) && (iCnChar<=45252)) {
+ 
+　　　　 return "A";
+ 
+　　}
+ 
+　　else if ((iCnChar>=45253) && (iCnChar<=45760)) {
+ 
+　　　　return "B";
+ 
+　　} else if ((iCnChar>=45761) && (iCnChar<=46317)) {
+ 
+　　　　return "C";
+ 
+　　} else if ((iCnChar>=46318) && (iCnChar<=46825)) {
+ 
+　　　　return "D";
+ 
+　　} else if ((iCnChar>=46826) && (iCnChar<=47009)) {
+ 
+　　　　return "E";
+ 
+　　} else if ((iCnChar>=47010) && (iCnChar<=47296)) {
+ 
+　　　　return "F";
+ 
+　　} else if ((iCnChar>=47297) && (iCnChar<=47613)) {
+ 
+　　　　return "G";
+ 
+　　} else if ((iCnChar>=47614) && (iCnChar<=48118)) {
+ 
+　　　　return "H";
+ 
+　　} else if ((iCnChar>=48119) && (iCnChar<=49061)) {
+ 
+　　　　return "J";
+ 
+　　} else if ((iCnChar>=49062) && (iCnChar<=49323)) {
+ 
+　　　　return "K";
+ 
+　　} else if ((iCnChar>=49324) && (iCnChar<=49895)) {
+ 
+　　　　return "L";
+ 
+　　} else if ((iCnChar>=49896) && (iCnChar<=50370)) {
+ 
+　　　　return "M";
+ 
+　　}else if ((iCnChar>=50371) && (iCnChar<=50613)) {
+ 
+　　　　return "N";
+ 
+　　} else if ((iCnChar>=50614) && (iCnChar<=50621)) {
+ 
+　　　　return "O";
+ 
+　　} else if ((iCnChar>=50622) && (iCnChar<=50905)) {
+ 
+　　　　return "P";
+ 
+　　} else if ((iCnChar>=50906) && (iCnChar<=51386)) {
+ 
+　　　　return "Q";
+ 
+　　} else if ((iCnChar>=51387) && (iCnChar<=51445)) {
+ 
+　　　　return "R";
+ 
+　　} else if ((iCnChar>=51446) && (iCnChar<=52217)) {
+ 
+　　　　return "S";
+ 
+　　} else if ((iCnChar>=52218) && (iCnChar<=52697)) {
+ 
+　　　　return "T";
+ 
+　　} else if ((iCnChar>=52698) && (iCnChar<=52979)) {
+ 
+　　　　return "W";
+ 
+　　} else if ((iCnChar>=52980) && (iCnChar<=53640)) {
+ 
+　　　　return "X";
+ 
+　　} else if ((iCnChar>=53689) && (iCnChar<=54480)) {
+ 
+　　　　return "Y";
+ 
+　　} else if ((iCnChar>=54481) && (iCnChar<=55289)) {
+ 
+　　　　return "Z";
+ 
+　　} else
+ 
+　　return ("?");
+ 
+}
+}
+
+
